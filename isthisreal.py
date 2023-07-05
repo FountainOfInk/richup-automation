1a254501b4e9faeadce363d380f20148c42a419f
FountainOfInk <glassfuture@protonmail.com>
2023-07-05 00:40:01 -0700

    hi!

--- isthisreal.py
+++ isthisreal.py
@@ -4,22 +4,35 @@ import time
 
 class Game:
     room_id: str
+    turn_number: int
+
+    def __init__(self) -> None:
+        pass
 
 class playerBot:
     conn: websocket.WebSocketApp
     name: str
     appearance: str
     uid: str
+    # every N turns it will our turn, what is N?        
+    my_turn_every: int
     game: Game
 
     def __init__(self, conn, room_id, name, appearance) -> None:
         self.conn = conn
-        self.room_id = room_id
         self.name = name
         self.appearance = appearance
+
+
+        self.game = Game()
+        self.game.room_id = room_id
+
+        join_game(conn, room_id, name, appearance)
+
+
     
         
-def join_game(conn: websocket.WebSocketApp, room_id: str, name: str, appearance: str = "#FFC73F") -> str:
+def join_game(conn: websocket.WebSocketApp, room_id: str, name: str, appearance: str = "#FFC73F") -> Game:
     # returns the SID (of the connection?)
     conn.send("40/api/game,")
     time.sleep(0.5)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import websocket
 import time
+import json
 import rel
-from isthisreal import join_game
+from isthisreal import join_game, playerBot
 
 # TODO: create a thread to keep the connection alive
 #       for both the websocket, and through the HTTP
@@ -11,14 +12,20 @@ headers = {
     'Cookie': "connect.sid=s%3AY7EkQmqEKraz0HrspU-CDAr6PKr1whPd.l2UpbMP6rysUKlT%2Fj69vPudyDVmkVOm4Qt5FDUi%2B2Ak; __ppIdCC=rixgup_io210883588.43.4; _ga_WE0S6HYRRZ=GS1.1.1688358895.1.1.1688359572.60.0.0; _ga=GA1.2.186912632.1688358896; usprivacy=1YYN; _pbjs_userid_consent_data=3524755945110770; sharedid=738d5254-d80f-420e-9d2c-712b4e7e9efa; __gads=ID=44e6ba0e4763a2e1:T=1688358904:RT=1688358904:S=ALNI_MYXKuvyFHsFnSzM20EhHjCtJVEsiA; __gpi=UID=000009a2eac2ac0f:T=1688358904:RT=1688358904:S=ALNI_MYwBviDPxYHE0kVstKk1VeUh8K00A; _cc_id=5f77588038484baba9a1e179d95095e8; panoramaId_expiry=1688963707952; panoramaId=7a1f69805ee2d423cce983723752bd9563ce17f28a657f9523af3d4fdae490b4; panoramaIdType=panoIndiv; cto_bundle=FxcxSF9vcjREOHhMUEdtd0dWaCUyRjFwdTJ5S3kwSVhNQzg1MFVyY1B3UkVJM041ZVk3U2dPUlVqS1RhNmpKVEw4OXkzaVBPOEhQOWY0VXJvdDlSUjQ0ZzROZDlBSk5HTDFMbGdwRWl1YXp0SW5xSzRSSiUyQlhLMW9hVHdMMmREUzVHdyUyQnNQNjZDVmRVWGxHaFEwJTJGdE1SWHZ2YmE0ZyUzRCUzRA; CountryCode=US; userFromEEA=false"
 }
 
-# every N turns it will our turn
-turn = 0
+bot: playerBot
 
 def on_message(ws: websocket.WebSocket, message):
     if message == "2":
         ws.send("3")
-    elif "game-started" in message:
-        pass
+    elif "entered-room" in message:
+        room_data = json.loads(message[len("42/api/game,"):])[1]["room"]
+        bot.game.turn_number = room_data["stats"]["turnsCount"]
+        if room_data["id"] != bot.game.room_id:
+            raise ValueError
+        for participant in room_data["participants"]:
+            if participant["name"] == bot.name:
+                bot.uid = participant["id"]
+    
 
 
 def on_error(ws, error):
@@ -32,7 +39,7 @@ def on_open(ws):
     join_game(ws, "juxdq", "jej")
 
 if __name__ == "__main__":
-    websocket.enableTrace(True)
+    # websocket.enableTrace(True)
     ws = websocket.WebSocketApp("wss://richup.io/socket.io/?EIO=4&transport=websocket", header=headers,
                               on_open=on_open,
                               on_message=on_message,
