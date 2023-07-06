b3db355492ba7f1ec08580c314739bab1c7e925a
FountainOfInk <glassfuture@protonmail.com>
2023-07-05 23:54:25 -0700

    Bot now rolls dice and ends its turn, is now able to be played with. Begin adding functionality for re-joining mid-game.

--- isthisreal.py
+++ isthisreal.py
@@ -8,11 +8,11 @@ GAME_FINISHED = 2
 class Game:
     room_id: str
     current_turn: int
-    state: bool
+    state: int
 
     def __init__(self, room_id) -> None:
         self.room_id = room_id
-        self.state = False
+        self.state = 0
 
 class playerBot:
     conn: websocket.WebSocketApp
@@ -23,36 +23,43 @@ class playerBot:
     my_turn: int
     game: Game
 
+    can_roll_dice: int
+
     def __init__(self, conn, room_id, name, appearance = "#FFC73F") -> None:
         self.conn = conn
         self.name = name
         self.appearance = appearance
         self.uid = None
-
+        self.can_roll_dice = True
 
         self.game = Game(room_id)
-
-        self.join_game(conn, room_id, name, appearance)
+        self.enter_room()
+        if not self.game.state == GAME_IN_PROGRESS:
+            self.join_game()
 
         
-    def join_game(self):
+    def enter_room(self):
         # returns the SID (of the connection?)
         self.conn.send("40/api/game,")
-        time.sleep(0.5)
+        time.sleep(0.2)
 
         # returns a large (>12kb) json message detailing all the info of the game's current state:
         # all the properties and all their prices, if an auction is going on, who's the top bidder,
         # all the chance cards and their effects, etc. etc.
         self.conn.send('42/api/game,["enter-room",{"roomId":"%s"}]' % self.game.room_id)
-        time.sleep(0.5)
+        time.sleep(0.2)
 
+    def join_game(self):
         # returns a message containing all the information about the player that just joined (you!)
         self.conn.send('42/api/game,["join-game",{"name":"%s","appearance":"%s"}]' % (self.name, self.appearance))
-        time.sleep(0.5)
+        time.sleep(0.2)
 
 
     def roll_dice(self):
-        self.conn.send()
+        self.conn.send('42/api/game,["plz-roll-dices"]')
+    
+    def end_turn(self):
+        self.conn.send('42/api/game,["end-turn"]')
 
     def leave(self):
         self.conn.close()
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import websocket
 import time
 import json
 import rel
-from isthisreal import join_game, playerBot
+from isthisreal import playerBot
 import isthisreal
 
 # TODO: create a thread to keep the connection alive
@@ -18,15 +18,29 @@ bot: playerBot = None
 def on_message(ws: websocket.WebSocket, message):
     if message == "2":
         ws.send("3")
+        return
 
     elif "joined-game" in message:
         self_data = json.loads(message[len("42/api/game,"):])[1]["selfPlayer"]
         bot.uid = self_data["id"]
 
+    # might create a struct representing the game 
+    # automatically created from a json example
+    # and just load data from that
     elif "entered-room" in message:
+        print("hello!!!")
         room_data = json.loads(message[len("42/api/game,"):])[1]["room"]
-        bot.game.current_turn = room_data["stats"]["turnsCount"]
         bot.game.state = room_data["state"]
+        bot.game.current_turn = room_data["stats"]["turnsCount"]
+
+        if room_data["state"] == isthisreal.GAME_IN_PROGRESS:
+            bot.uid = room_data['selfParticipantId']
+            for i,player in enumerate(room_data["players"]):
+                print(f"player:{player['name']}, id:{player['id']}")
+                print(f"me: {bot.name}, id: {bot.uid}")
+                if player["id"] == bot.uid:
+                    bot.my_turn = i+1
+
         if room_data["id"] != bot.game.room_id:
             raise ValueError
 
@@ -37,30 +51,49 @@ def on_message(ws: websocket.WebSocket, message):
                 bot.my_turn = i+1
         bot.game.current_turn = 1
         bot.game.state = isthisreal.GAME_IN_PROGRESS
+    
 
     if bot.game.state == isthisreal.GAME_IN_PROGRESS:
-        print(f"game state:{bot.game.state} game_in_progress:{isthisreal.GAME_IN_PROGRESS}")
+        #print(f"it is the {bot.game.current_turn}th turn, and my turn is every {bot.my_turn} moves")
         # M % N = 0, where M is some multiple of N
         # so if it's the 9th turn and we are the 3rd
         # player in order, it's our turn (for the third time)
         if bot.game.current_turn % bot.my_turn == 0:
-            print("my turn!")
+            #print("my turn!")
+            if "dice-rolled" in message:
+                dice_data = json.loads(message[len("42/api/game,"):])[1]["dice"]
+                doubles = dice_data[0] == dice_data[1]
+                bot.can_roll_dice = doubles
+
+            if bot.can_roll_dice:
+                #print("i can roll the dice, rolling")
+                bot.roll_dice()
+            else:
+                #print("i cannot roll the dice, ending my turn")
+                bot.can_roll_dice = True
+                bot.end_turn()
+
+        if "turn-ended" in message:
+            bot.game.current_turn += 1
         
 
 
 def on_error(ws, error):
-    print(error)
+    # print(error)
+    pass
 
 def on_close(ws, close_status_code, close_msg):
-    print("### closed ###")
+    #print("### closed ###")
+    pass
 
+# MAIN() is here!!
 def on_open(ws):
-    print("Opened connection")
+    #print("Opened connection")
     global bot
-    bot = playerBot(ws, "fgbxg", "amonger")
+    bot = playerBot(ws, "5q3pk", "amonger")
 
 if __name__ == "__main__":
-    # websocket.enableTrace(True)
+    # websocket.enableTrace(True, level="trace")
     ws = websocket.WebSocketApp("wss://richup.io/socket.io/?EIO=4&transport=websocket", header=headers,
                               on_open=on_open,
                               on_message=on_message,
