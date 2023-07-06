a6d2599b816657619df21a5a21a3211538c69589
FountainOfInk <glassfuture@protonmail.com>
2023-07-05 21:23:33 -0700

    Merged `join_game` into the playerBot class, added a `state` to Game, representing if the game is in the lobby, in progress, or on the finished screen; Added logic to determine player order and if it is currently the bot's turn

--- isthisreal.py
+++ isthisreal.py
@@ -1,13 +1,18 @@
 import websocket
 import time
 
+GAME_LOBBY = 0
+GAME_IN_PROGRESS = 1
+GAME_FINISHED = 2
 
 class Game:
     room_id: str
-    turn_number: int
+    current_turn: int
+    state: bool
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, room_id) -> None:
+        self.room_id = room_id
+        self.state = False
 
 class playerBot:
     conn: websocket.WebSocketApp
@@ -15,40 +20,39 @@ class playerBot:
     appearance: str
     uid: str
     # every N turns it will our turn, what is N?        
-    my_turn_every: int
+    my_turn: int
     game: Game
 
-    def __init__(self, conn, room_id, name, appearance) -> None:
+    def __init__(self, conn, room_id, name, appearance = "#FFC73F") -> None:
         self.conn = conn
         self.name = name
         self.appearance = appearance
+        self.uid = None
 
 
-        self.game = Game()
-        self.game.room_id = room_id
+        self.game = Game(room_id)
 
-        join_game(conn, room_id, name, appearance)
+        self.join_game(conn, room_id, name, appearance)
 
-
-    
         
-def join_game(conn: websocket.WebSocketApp, room_id: str, name: str, appearance: str = "#FFC73F") -> Game:
-    # returns the SID (of the connection?)
-    conn.send("40/api/game,")
-    time.sleep(0.5)
+    def join_game(self):
+        # returns the SID (of the connection?)
+        self.conn.send("40/api/game,")
+        time.sleep(0.5)
 
-    # returns a large (>12kb) json message detailing all the info of the game's current state:
-    # all the properties and all their prices, if an auction is going on, who's the top bidder,
-    # all the chance cards and their effects, etc. etc.
-    conn.send('42/api/game,["enter-room",{"roomId":"%s"}]' % room_id)
-    time.sleep(0.5)
+        # returns a large (>12kb) json message detailing all the info of the game's current state:
+        # all the properties and all their prices, if an auction is going on, who's the top bidder,
+        # all the chance cards and their effects, etc. etc.
+        self.conn.send('42/api/game,["enter-room",{"roomId":"%s"}]' % self.game.room_id)
+        time.sleep(0.5)
 
-    # returns a message containing all the information about the player that just joined (you!)
-    conn.send('42/api/game,["join-game",{"name":"%s","appearance":"%s"}]' % (name, appearance))
-    time.sleep(0.5)
+        # returns a message containing all the information about the player that just joined (you!)
+        self.conn.send('42/api/game,["join-game",{"name":"%s","appearance":"%s"}]' % (self.name, self.appearance))
+        time.sleep(0.5)
 
 
-def roll_dice():
-    pass
-def terminate(conn: websocket.WebSocket):
-    conn.close()
+    def roll_dice(self):
+        self.conn.send()
+
+    def leave(self):
+        self.conn.close()
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import json
 import rel
 from isthisreal import join_game, playerBot
+import isthisreal
 
 # TODO: create a thread to keep the connection alive
 #       for both the websocket, and through the HTTP
@@ -12,20 +13,39 @@ headers = {
     'Cookie': "connect.sid=s%3AY7EkQmqEKraz0HrspU-CDAr6PKr1whPd.l2UpbMP6rysUKlT%2Fj69vPudyDVmkVOm4Qt5FDUi%2B2Ak; __ppIdCC=rixgup_io210883588.43.4; _ga_WE0S6HYRRZ=GS1.1.1688358895.1.1.1688359572.60.0.0; _ga=GA1.2.186912632.1688358896; usprivacy=1YYN; _pbjs_userid_consent_data=3524755945110770; sharedid=738d5254-d80f-420e-9d2c-712b4e7e9efa; __gads=ID=44e6ba0e4763a2e1:T=1688358904:RT=1688358904:S=ALNI_MYXKuvyFHsFnSzM20EhHjCtJVEsiA; __gpi=UID=000009a2eac2ac0f:T=1688358904:RT=1688358904:S=ALNI_MYwBviDPxYHE0kVstKk1VeUh8K00A; _cc_id=5f77588038484baba9a1e179d95095e8; panoramaId_expiry=1688963707952; panoramaId=7a1f69805ee2d423cce983723752bd9563ce17f28a657f9523af3d4fdae490b4; panoramaIdType=panoIndiv; cto_bundle=FxcxSF9vcjREOHhMUEdtd0dWaCUyRjFwdTJ5S3kwSVhNQzg1MFVyY1B3UkVJM041ZVk3U2dPUlVqS1RhNmpKVEw4OXkzaVBPOEhQOWY0VXJvdDlSUjQ0ZzROZDlBSk5HTDFMbGdwRWl1YXp0SW5xSzRSSiUyQlhLMW9hVHdMMmREUzVHdyUyQnNQNjZDVmRVWGxHaFEwJTJGdE1SWHZ2YmE0ZyUzRCUzRA; CountryCode=US; userFromEEA=false"
 }
 
-bot: playerBot
+bot: playerBot = None
 
 def on_message(ws: websocket.WebSocket, message):
     if message == "2":
         ws.send("3")
+
+    elif "joined-game" in message:
+        self_data = json.loads(message[len("42/api/game,"):])[1]["selfPlayer"]
+        bot.uid = self_data["id"]
+
     elif "entered-room" in message:
         room_data = json.loads(message[len("42/api/game,"):])[1]["room"]
-        bot.game.turn_number = room_data["stats"]["turnsCount"]
+        bot.game.current_turn = room_data["stats"]["turnsCount"]
+        bot.game.state = room_data["state"]
         if room_data["id"] != bot.game.room_id:
             raise ValueError
-        for participant in room_data["participants"]:
-            if participant["name"] == bot.name:
-                bot.uid = participant["id"]
-    
+
+    elif "game-started" in message:
+        game_data = json.loads(message[len("42/api/game,"):])[1]
+        for i,player in enumerate(game_data["participantsOrder"]):
+            if player["id"] == bot.uid:
+                bot.my_turn = i+1
+        bot.game.current_turn = 1
+        bot.game.state = isthisreal.GAME_IN_PROGRESS
+
+    if bot.game.state == isthisreal.GAME_IN_PROGRESS:
+        print(f"game state:{bot.game.state} game_in_progress:{isthisreal.GAME_IN_PROGRESS}")
+        # M % N = 0, where M is some multiple of N
+        # so if it's the 9th turn and we are the 3rd
+        # player in order, it's our turn (for the third time)
+        if bot.game.current_turn % bot.my_turn == 0:
+            print("my turn!")
+        
 
 
 def on_error(ws, error):
@@ -36,7 +56,8 @@ def on_close(ws, close_status_code, close_msg):
 
 def on_open(ws):
     print("Opened connection")
-    join_game(ws, "juxdq", "jej")
+    global bot
+    bot = playerBot(ws, "fgbxg", "amonger")
 
 if __name__ == "__main__":
     # websocket.enableTrace(True)
